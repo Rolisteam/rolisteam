dd4b20115a5cfa91f778e83019d7e37a7c500bac
Renaud G <renaud@rolisteam.org>
2015-02-18 07:21:21 +0100

    Import revision 220 from SVN: ------------------------------------------------------------------------ r220 | jogo@matabio.net | 2011-07-08 22:36:44 +0200 (ven., 08 juil. 2011) | 2 lines
    [branch 1.0.0] Oups... I forgot to "svn add" new files.
    
    ------------------------------------------------------------------------

---
+++ src/localpersonmodel.cpp
@@ -0,0 +1,117 @@
+/*************************************************************************
+ *   Copyright (C) 2011 by Joseph Boudou                                 *
+ *                                                                       *
+ *   http://www.rolisteam.org/                                           *
+ *                                                                       *
+ *   Rolisteam is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published   *
+ *   by the Free Software Foundation; either version 2 of the License,   *
+ *   or (at your option) any later version.                              *
+ *                                                                       *
+ *   This program is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *   GNU General Public License for more details.                        *
+ *                                                                       *
+ *   You should have received a copy of the GNU General Public License   *
+ *   along with this program; if not, write to the                       *
+ *   Free Software Foundation, Inc.,                                     *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           *
+ *************************************************************************/
+
+
+#include "localpersonmodel.h"
+
+#include "persons.h"
+#include "playersList.h"
+
+
+LocalPersonModel::LocalPersonModel()
+    : PlayersListProxyModel()
+{
+}
+
+LocalPersonModel & LocalPersonModel::instance()
+{
+    static LocalPersonModel s_model;
+    return s_model;
+}
+
+QModelIndex LocalPersonModel::mapFromSource(const QModelIndex & sourceIndex) const
+{
+    if (!sourceIndex.isValid())
+        return QModelIndex();
+
+    quint32 parentRow = (quint32)(sourceIndex.internalId() & PlayersList::NoParent);
+    switch (parentRow)
+    {
+        case 0:
+            return createIndex(sourceIndex.row() + 1, sourceIndex.column(), 0);
+        case PlayersList::NoParent:
+            if (sourceIndex.row() == 0)
+                return createIndex(0, sourceIndex.column(), parentRow);
+        default:
+            return QModelIndex();
+    }
+}
+
+QModelIndex LocalPersonModel::mapToSource(const QModelIndex & proxyIndex) const
+{
+    if (!proxyIndex.isValid())
+        return QModelIndex();
+
+    PlayersList & g_playersList = PlayersList::instance();
+    quint32 parentRow = (quint32)(proxyIndex.internalId() & PlayersList::NoParent);
+    if (parentRow == 0)
+    {
+        return g_playersList.mapIndexToMe(createIndex(proxyIndex.row() - 1, proxyIndex.column(), parentRow));
+    }
+    return g_playersList.mapIndexToMe(proxyIndex);
+}
+
+
+QVariant LocalPersonModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || role == Qt::DecorationRole)
+        return QVariant();
+    return QAbstractProxyModel::data(index, role);
+}
+
+QModelIndex LocalPersonModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (parent.isValid() || column != 0)
+        return QModelIndex();
+
+    QModelIndex sourceLocalPlayer = sourceModel()->index(0, 0, QModelIndex());
+    if (row == 0)
+        return mapFromSource(sourceLocalPlayer);
+    else
+        return mapFromSource(sourceModel()->index(row - 1, 0, sourceLocalPlayer));
+}
+
+QModelIndex LocalPersonModel::parent(const QModelIndex &index) const
+{
+    Q_UNUSED(index);
+    return QModelIndex();
+}
+
+int LocalPersonModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+    return 1 + PlayersList::instance().localPlayer()->getCharactersCount();
+}
+
+bool LocalPersonModel::filterChangingRows(QModelIndex & parent, int & start, int & end)
+{
+    if (parent.isValid() && parent.row() == 0)
+    {
+        parent = QModelIndex();
+        start += 1;
+        end   += 1;
+        qDebug("LPM filterChangingRows true %d %d", start, end);
+        return true;
+    }
+    qDebug("LPM filterChangingRows false %d %d", start, end);
+    return false;
+}
---
+++ src/localpersonmodel.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+ *   Copyright (C) 2011 by Joseph Boudou                                 *
+ *                                                                       *
+ *   http://www.rolisteam.org/                                           *
+ *                                                                       *
+ *   Rolisteam is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published   *
+ *   by the Free Software Foundation; either version 2 of the License,   *
+ *   or (at your option) any later version.                              *
+ *                                                                       *
+ *   This program is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *   GNU General Public License for more details.                        *
+ *                                                                       *
+ *   You should have received a copy of the GNU General Public License   *
+ *   along with this program; if not, write to the                       *
+ *   Free Software Foundation, Inc.,                                     *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           *
+ *************************************************************************/
+
+
+#ifndef LOCAL_PERSON_MODEL_H
+#define LOCAL_PERSON_MODEL_H
+
+#include "playerslistproxy.h"
+
+class LocalPersonModel : public PlayersListProxyModel
+{
+    Q_OBJECT
+
+    public:
+        static LocalPersonModel & instance();
+        
+        QModelIndex mapFromSource(const QModelIndex & sourceIndex) const;
+        QModelIndex mapToSource(const QModelIndex & proxyIndex) const;
+
+        QVariant data(const QModelIndex &index, int role) const;
+        QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
+        QModelIndex parent(const QModelIndex &index) const;
+        int rowCount(const QModelIndex &parent = QModelIndex()) const;
+
+    protected:
+        bool filterChangingRows(QModelIndex & parent, int & start, int & end);
+
+    private:
+        LocalPersonModel();
+};
+
+#endif
