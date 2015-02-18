d4bedde014d3bd389d5d7fb66c2e4806339199ef
Renaud G <renaud@rolisteam.org>
2015-02-18 07:11:09 +0100

    Import revision 163 from SVN: ------------------------------------------------------------------------ r163 | renaud.guezennec@gmail.com | 2011-02-13 11:38:33 +0100 (dim., 13 févr. 2011) | 4 lines
    [branch 1.0.0]
    -use qassistant for documentation if does not work QDesktop stuff is
    used.
    
    ------------------------------------------------------------------------

--- src/datareader.cpp
+++ src/datareader.cpp
@@ -21,8 +21,6 @@
 
 #include "datareader.h"
 
-#include "string.h"
-
 DataReader::DataReader(quint32 bufferSize, const char * buffer)
 {
     m_buffer = new char[bufferSize];
@@ -43,17 +41,15 @@ DataReader::reset()
     m_pos = m_buffer;
 }
 
-int
-DataReader::left() const
+int DataReader::left() const
 {
     return (m_end - m_pos);
 }
 
 
-quint8
-DataReader::uint8()
+quint8 DataReader::uint8()
 {
-    size_t size = sizeof(quint8);
+    quint8 size = sizeof(quint8);
     if (left() >= size)
     {
         quint8 ret = (quint8) *m_pos;
@@ -63,8 +59,7 @@ DataReader::uint8()
     return 0;
 }
 
-QString
-DataReader::string8()
+QString DataReader::string8()
 {
     int sizeQChar = uint8();
     int sizeBytes = sizeQChar * sizeof(QChar);
