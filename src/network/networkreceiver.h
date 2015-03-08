b7a85355ac0a3f45ff1d35227a9349c68530b8a2
Renaud G <renaud@rolisteam.org>
2015-03-08 18:39:26 +0100

    -new API for receiving message from network: networkreceiver.h -just need to implement the processMessage function.

--- src/network/networkreceiver.h
+++ src/network/networkreceiver.h
@@ -1,4 +1,12 @@
 #ifndef NETWORKRECEIVER_H
 #define NETWORKRECEIVER_H
 
+#include "network/networkmessagereader.h"
+
+class NetWorkReceiver
+{
+public:
+    virtual void processMessage(NetworkMessageReader* msg) = 0;
+};
+
 #endif // NETWORKRECEIVER_H
