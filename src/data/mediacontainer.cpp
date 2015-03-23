d693bf88e739482bb10b73359bf1ad90340de9d1
Renaud G <renaud@rolisteam.org>
2015-03-23 08:41:56 +0100

    -add hiérarchie father for all media: map and image.

---
+++ src/data/mediacontainer.cpp
@@ -0,0 +1,5 @@
+#include "mediacontainer.h"
+
+MediaContainer::MediaContainer()
+{
+}
---
+++ src/data/mediacontainer.h
@@ -0,0 +1,10 @@
+#ifndef MEDIACONTAINER_H
+#define MEDIACONTAINER_H
+
+class MediaContainer
+{
+public:
+    MediaContainer();
+};
+
+#endif // MEDIACONTAINER_H
