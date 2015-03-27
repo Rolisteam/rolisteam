8e59af2eaf5d0588ce268a050274abc9f25a6814
Renaud G <renaud@rolisteam.org>
2015-03-27 08:27:28 +0100

    first implementation of CleverURI

--- src/data/cleveruri.cpp
+++ src/data/cleveruri.cpp
@@ -1,5 +1,159 @@
+/***************************************************************************
+    *	Copyright (C) 2009 by Renaud Guezennec                             *
+    *   http://renaudguezennec.homelinux.org/accueil,3.html                   *
+    *                                                                         *
+    *   Rolisteam is free software; you can redistribute it and/or modify     *
+    *   it under the terms of the GNU General Public License as published by  *
+    *   the Free Software Foundation; either version 2 of the License, or     *
+    *   (at your option) any later version.                                   *
+    *                                                                         *
+    *   This program is distributed in the hope that it will be useful,       *
+    *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+    *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+    *   GNU General Public License for more details.                          *
+    *                                                                         *
+    *   You should have received a copy of the GNU General Public License     *
+    *   along with this program; if not, write to the                         *
+    *   Free Software Foundation, Inc.,                                       *
+    *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+    ***************************************************************************/
 #include "cleveruri.h"
+#include <QString>
+#include <QDebug>
+
+
+#include <QDataStream>
+
+/////////////////
+// CleverUri
+/////////////////
+QString CleverURI::m_textIcon=QString(":/resources/icons/notes.png");
+QString CleverURI::m_mapIcon=QString(":/resources/icons/map.png");
+QString CleverURI::m_pictureIcon=QString(":/resources/icons/image.png");
+QString CleverURI::m_charactersheetIcon=QString(":/resources/icons/treeview.png");
+QString CleverURI::m_scenarioIcon=QString(":/resources/icons/scenario.png");
+QString CleverURI::m_chatIcon=QString(":/resources/icons/scenario.png");
+QString CleverURI::m_musicIcon=QString(":/resources/icons/music.svg");
+#ifdef WITH_PDF
+QString CleverURI::m_pdfIcon=QString(":/iconpdf");
+#endif
+QString CleverURI::m_empty=QString("");
 
 CleverURI::CleverURI()
 {
+
+}
+
+CleverURI::CleverURI(const CleverURI & mp)
+{
+    m_type=mp.getType();
+    m_uri=mp.getUri();
+    defineShortName();
+}
+QString& CleverURI::getIcon(ContentType type)
+{
+    switch(type)
+    {
+    case CleverURI::CHARACTERSHEET:
+        return m_charactersheetIcon;
+        break;
+    case CleverURI::PICTURE:
+        return m_pictureIcon;
+        break;
+    case CleverURI::MAP:
+        return m_mapIcon;
+        break;
+    case CleverURI::TCHAT:
+        return m_chatIcon;
+        break;
+    case CleverURI::TEXT:
+        return m_textIcon;
+        break;
+    case CleverURI::SCENARIO:
+        return m_scenarioIcon;
+        break;
+    case CleverURI::SONG:
+        return m_musicIcon;
+        break;
+#ifdef WITH_PDF
+    case CleverURI::PDF:
+        return m_pdfIcon;
+        break;
+#endif
+    default:
+        return m_empty;
+        break;
+    }
+}
+
+CleverURI::CleverURI(QString uri,ContentType type)
+    : m_uri(uri),m_type(type)
+{
+    defineShortName();
+}
+
+CleverURI::~CleverURI()
+{
+    // qDebug() << "Destructor of CleverURI";
+}
+bool CleverURI::operator==(const CleverURI& uri) const
+{
+    if((uri.getUri()==getUri())&&(uri.getType()==getType()))
+        return true;
+    return false;
+}
+
+void CleverURI::setUri(QString& uri)
+{
+    m_uri=uri;
+    defineShortName();
+}
+
+void CleverURI::setType(CleverURI::ContentType type)
+{
+    m_type=type;
+}
+
+const QString CleverURI::getUri() const
+{
+    return m_uri;
+}
+
+CleverURI::ContentType CleverURI::getType() const
+{
+    return m_type;
+}
+bool CleverURI::hasChildren() const
+{
+    return false;
+}
+void CleverURI::defineShortName()
+{
+    m_shortname = m_uri.mid(m_uri.lastIndexOf('/')+1);
+}
+
+const QString& CleverURI::getShortName() const
+{
+    return m_shortname;
+}
+void CleverURI::setShortName(QString& name)
+{
+    m_shortname = name;
+}
+
+QDataStream& operator<<(QDataStream& out, const CleverURI& con)
+{
+    out << con.getUri();
+    out << (int)con.getType();
+    return out;
+}
+
+QDataStream& operator>>(QDataStream& is,CleverURI& con)
+{
+    is >>(con.m_uri);
+    int type;
+    is >> type;
+    con.m_type=(CleverURI::ContentType)type;
+    con.defineShortName();
+    return is;
 }
--- src/data/cleveruri.h
+++ src/data/cleveruri.h
@@ -1,10 +1,146 @@
+/***************************************************************************
+    *	Copyright (C) 2009 by Renaud Guezennec                             *
+    *   http://renaudguezennec.homelinux.org/accueil,3.html                   *
+    *                                                                         *
+    *   Rolisteam is free software; you can redistribute it and/or modify     *
+    *   it under the terms of the GNU General Public License as published by  *
+    *   the Free Software Foundation; either version 2 of the License, or     *
+    *   (at your option) any later version.                                   *
+    *                                                                         *
+    *   This program is distributed in the hope that it will be useful,       *
+    *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+    *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+    *   GNU General Public License for more details.                          *
+    *                                                                         *
+    *   You should have received a copy of the GNU General Public License     *
+    *   along with this program; if not, write to the                         *
+    *   Free Software Foundation, Inc.,                                       *
+    *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+    ***************************************************************************/
 #ifndef CLEVERURI_H
 #define CLEVERURI_H
 
+
+#include <QString>
+#include <QMetaType>
+#include "ressourcesnode.h"
+
+/**
+    * @brief This class stores data refering to file, uri and type
+    * @brief as a file can be loaded as different type.
+    * @brief E.g: an image can be loaded as Picture or as Background for map.
+    *
+    */
+
 class CleverURI : public RessourcesNode
 {
+
 public:
+    /**
+    * @brief enum of all available type.
+    *
+    */
+    enum ContentType {MAP,TCHAT,PICTURE,TEXT,CHARACTERSHEET,SCENARIO,SONG
+              #ifdef WITH_PDF
+                      ,PDF
+              #endif
+                     };
+    /**
+    * @brief default constructor
+    *
+    */
     CleverURI();
+    /**
+    * @brief copy constructor
+    *
+    */
+    CleverURI(const CleverURI & mp);
+    /**
+    * @brief constructor with parameter
+    *
+    */
+    CleverURI(QString uri,ContentType type);
+    /**
+    * @brief Destructor
+    *
+    */
+    virtual ~CleverURI();
+
+    /**
+    * @brief set the URI parameter
+    * @param new uri
+    */
+    void setUri(QString& uri);
+    /**
+    * @brief set the type
+    * @param the new type
+    */
+    void setType(CleverURI::ContentType type);
+    /**
+    * @brief accessor to the URI
+    * @return the URI
+    */
+    const QString getUri() const;
+    /**
+    * @brief accessor to the type
+    * @return the type
+    */
+    CleverURI::ContentType getType() const;
+    bool operator==(const CleverURI& uri1) const;
+
+    /**
+    * @brief overriden method from RessourceNode
+    * @return always false
+    */
+    bool hasChildren() const;
+    /**
+    * @brief accessor to a shorter uri (only the filename)
+    * @return the filename
+    */
+    const QString& getShortName() const;
+    /**
+    * @brief change the short name
+    * @param new short name
+    */
+    virtual void setShortName(QString& name);
+    /**
+    * @brief static method which returns the appropriate icon path given the type
+    * @param type of the content
+    * @return the path to the icon
+    */
+    static QString& getIcon(ContentType type);
+
+private:
+    /**
+    * @brief split the uri to get the shortname
+    *
+    */
+    void defineShortName();
+
+
+
+
+    QString m_uri; ///< member to store the uri
+    CleverURI::ContentType m_type;///< member to store the content type
+    QString m_shortname; ///< member to store the shortname
+
+    static QString m_musicIcon; ///< static member to store the music icon
+    static QString m_textIcon;///< static member to store the text icon
+    static QString m_mapIcon; ///< static member to store the map icon
+    static QString m_pictureIcon; ///< static member to store the picture icon
+    static QString m_charactersheetIcon; ///< static member to store the character icon
+    static QString m_scenarioIcon; ///< static member to store the scenario icon
+    static QString m_chatIcon; ///< static member to store the chat icon
+#ifdef WITH_PDF
+    static QString m_pdfIcon;///< static member to store the pdf icon
+#endif
+    static QString m_empty;
+
+    friend QDataStream& operator<<(QDataStream& os,const CleverURI&); ///< operator for serialisation (writing)
+    friend QDataStream& operator>>(QDataStream& is,CleverURI&); ///< operator for serialisation (reading)
 };
+typedef QList<CleverURI> CleverUriList;
+Q_DECLARE_METATYPE(CleverURI)
+Q_DECLARE_METATYPE(CleverUriList)
 
 #endif // CLEVERURI_H
