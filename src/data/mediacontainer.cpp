0c243a6df553b998fc7247ef0eff0a25c57c505e
Renaud G <renaud@rolisteam.org>
2015-03-29 05:19:51 +0200

    -make MediaContainer a subclass of QMdiSubWindow.

--- src/data/mediacontainer.cpp
+++ src/data/mediacontainer.cpp
@@ -1,8 +1,17 @@
 #include "mediacontainer.h"
+#include <QMessageBox>
 
-MediaContainer::MediaContainer()
+
+MediaContainer::MediaContainer(QWidget* parent)
+    : QMdiSubWindow(parent),m_uri(NULL),m_preferences(PreferencesManager::getInstance())
+{
+    //m_preferences = ;
+    setAttribute(Qt::WA_DeleteOnClose,false);
+}
+MediaContainer::~MediaContainer()
 {
 }
+
 void MediaContainer::setLocalPlayerId(QString id)
 {
     m_localPlayerId = id;
@@ -20,7 +29,63 @@ QString MediaContainer::getTitle() const
 {
 	return m_title;
 }
+CleverURI*  MediaContainer::getCleverUri() const
+{
+    return  m_uri;
+}
 void MediaContainer::openMedia()
 {
 
+}
+void MediaContainer::error(QString err,QWidget* parent)
+{
+    if(NULL!=parent)
+    {
+        QMessageBox msgBox(parent);
+        msgBox.addButton(QMessageBox::Cancel);
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.setWindowTitle(QObject::tr("Loading error"));
+        msgBox.move(QPoint(parent->width()/2, parent->height()/2) + QPoint(-100, -50));
+
+        Qt::WindowFlags flags = msgBox.windowFlags();
+        msgBox.setWindowFlags(flags ^ Qt::WindowSystemMenuHint);
+
+        msgBox.setText(err);
+        msgBox.exec();
+    }
+}
+bool MediaContainer::isUriEndWith(QString str)
+{
+    if(NULL!=m_uri)
+    {
+        if(m_uri->getUri().endsWith(str))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void MediaContainer::setTitle(QString str)
+{
+  m_title = str;
+  setWindowTitle(m_title);
+}
+void MediaContainer::setVisible(bool b)
+{
+    if(NULL!=widget())
+    {
+        widget()->setVisible(b);
+        m_action->setChecked(b);
+    }
+    QMdiSubWindow::setVisible(b);
+}
+void MediaContainer::setAction(QAction* act)
+{
+    m_action = act;
+    connect(m_action, SIGNAL(triggered(bool)), this, SLOT(setVisible(bool)));
+}
+QAction* MediaContainer::getAction()
+{
+    return m_action;
 }
--- src/data/mediacontainer.h
+++ src/data/mediacontainer.h
@@ -4,20 +4,25 @@
 
 
 #include <QString>
+#include <QMdiSubWindow>
+#include <QAction>
 
 
 #include "data/cleveruri.h"
+#include "preferences/preferencesmanager.h"
 
 /**
  * @brief The MediaContainer class
  */
-class MediaContainer
+class MediaContainer : public QMdiSubWindow
 {
+    Q_OBJECT
 public:
     /**
      * @brief MediaContainer
      */
-    MediaContainer();
+    MediaContainer(QWidget* parent = NULL);
+    virtual ~MediaContainer();
 
     /**
      * @brief setLocalPlayerId
@@ -35,6 +40,10 @@ public:
 	 * @param uri
 	 */
 	virtual void setCleverUri(CleverURI* uri);
+
+    virtual CleverURI* getCleverUri() const;
+
+    virtual bool isUriEndWith(QString);
 
 	/**
 	 * @brief readFile
@@ -54,15 +63,40 @@ public:
 	 * @return
 	 */
 	virtual QString getTitle() const;
+    /**
+     * @brief setTitle
+     * @param str
+     */
+    virtual void setTitle(QString str);
+    /**
+     * @brief error
+     * @param err - error message to display
+     */
+    void error(QString err,QWidget* parent);
+    void setAction(QAction*);
+    QAction* getAction();
 
 
+signals:
+    /**
+     * @brief visibleChanged
+     */
+    void visibleChanged(bool);
+
+public slots:
+    /**
+     * @brief setVisible
+     * @param b
+     */
+    void setVisible(bool b);
 
 protected:
     QString m_localPlayerId;
 	CleverURI* m_uri;
 	QString m_title;
-
-
+    QString m_filter;
+    PreferencesManager* m_preferences;
+    QAction* m_action;
 };
 
 #endif // MEDIACONTAINER_H
