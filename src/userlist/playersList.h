b589b0d02686a38b515484890d4fd3df3ec46208
Renaud G <renaud@rolisteam.org>
2015-05-05 10:29:03 +0200

    organize the file

--- src/userlist/playersList.h
+++ src/userlist/playersList.h
@@ -42,6 +42,10 @@ class PlayersList : public QAbstractItemModel
     Q_OBJECT
 
 public:
+    enum ItemDataRole
+    {
+        IdentifierRole = Qt::UserRole
+    };
     /**
      * @brief Get the singleton
      */
@@ -60,51 +64,45 @@ public:
     bool setData(const QModelIndex &index, const QVariant &value, int role);
     void sendOffLocalPlayerInformations();
     void sendOffFeatures(Player* player);
+
     // Event handlers
     bool event(QEvent * event);
-
-    // Proxy helpers
-    static const quint32 NoParent = 0x7fffffff;
     QModelIndex mapIndexToMe(const QModelIndex & index) const;
-    enum ItemDataRole
-    {
-        IdentifierRole = Qt::UserRole
-    };
 
     // Getters
     Player* localPlayer() const;
     bool isLocal(Person * person) const;
-
     int numPlayers() const;
     Player* getPlayer(int index) const;
-
     Person* getPerson(const QString & uuid) const;
     Player* getPlayer(const QString & uuid) const;
     Character * getCharacter(const QString & uuid) const;
     Player* getLocalPlayer() const;
-    void cleanListButLocal();
-    void completeListClean();
     /**
-         * @brief Same as getPlayer(uuid), if getPerson(uuid) is a Player.
-         * Same as getPerson(uuid)->parent() if it's a Character.
-         */
+     * @brief Same as getPlayer(uuid), if getPerson(uuid) is a Player.
+     * Same as getPerson(uuid)->parent() if it's a Character.
+     */
     Player* getParent(const QString & uuid) const;
-
     Person* getPerson(const QModelIndex & index) const;
     Player* getPlayer(const QModelIndex & index) const;
     Character * getCharacter(const QModelIndex & index) const;
+
+
+    void cleanListButLocal();
+    void completeListClean();
     bool everyPlayerHasFeature(const QString & name, quint8 version = 0) const;
+
 
     // Setters
     void setLocalPlayer(Player * player);
     void addLocalCharacter(Character * newCharacter);
-
     void changeLocalPerson(Person * person, const QString & name, const QColor & color);
     void setLocalPersonName(Person * person, const QString & name);
     void setLocalPersonColor(Person * person, const QColor & color);
-
     void delLocalCharacter(int index);
 
+    // Proxy helpers
+    static const quint32 NoParent = 0x7fffffff;
 signals:
     void playerAdded(Player * player);
     void characterAdded(Character * character);
@@ -116,11 +114,42 @@ signals:
     void characterDeleted(Character * character);
     void playerAddedAsClient(Player *player);
     /**
-         * @brief Send when local is client and the server refused local player to be GM.
-         */
+     * @brief Send when local is client and the server refused local player to be GM.
+     */
     void localGMRefused();
 
 
+private slots:
+    void delPlayerWithLink(NetworkLink * link);
+    void sendDelLocalPlayer();
+
+private:
+    /**
+     * @brief Constructor
+     * @see instance()
+     */
+    PlayersList();
+
+    /**
+     * @brief Destructor
+     * @see instance()
+     */
+    ~PlayersList();
+    QModelIndex createIndex(Person * person) const;
+    void addPlayer(Player * player);
+    void addCharacter(Player * player, Character * character);
+    void delPlayer(Player * player);
+    void delCharacter(Player * parent, int index);
+    void addPlayer(NetworkMessageReader & data);
+    void addPlayerAsServer(ReceiveEvent * event);
+    void delPlayer(NetworkMessageReader & data);
+    void setPersonName(NetworkMessageReader & data);
+    void setPersonColor(NetworkMessageReader & data);
+    void addCharacter(NetworkMessageReader & data);
+    void delCharacter(NetworkMessageReader & data);
+    bool p_setLocalPersonName(Person * person, const QString & name);
+    bool p_setLocalPersonColor(Person * person, const QColor & color);
+    void notifyPersonChanged(Person * person);
 
 private:
     QList<Player *> m_playersList;
@@ -130,43 +159,6 @@ private:
     static PlayersList* m_singleton;
 
     int m_gmCount;
-
-    /**
-         * @brief Constructor
-         * @see instance()
-         */
-    PlayersList();
-
-    /**
-         * @brief Destructor
-         * @see instance()
-         */
-    ~PlayersList();
-
-    QModelIndex createIndex(Person * person) const;
-
-    void addPlayer(Player * player);
-    void addCharacter(Player * player, Character * character);
-
-    void delPlayer(Player * player);
-    void delCharacter(Player * parent, int index);
-
-    void addPlayer(NetworkMessageReader & data);
-    void addPlayerAsServer(ReceiveEvent * event);
-    void delPlayer(NetworkMessageReader & data);
-    void setPersonName(NetworkMessageReader & data);
-    void setPersonColor(NetworkMessageReader & data);
-    void addCharacter(NetworkMessageReader & data);
-    void delCharacter(NetworkMessageReader & data);
-
-    bool p_setLocalPersonName(Person * person, const QString & name);
-    bool p_setLocalPersonColor(Person * person, const QColor & color);
-
-    void notifyPersonChanged(Person * person);
-
-private slots:
-    void delPlayerWithLink(NetworkLink * link);
-    void sendDelLocalPlayer();
 };
 
 #endif
