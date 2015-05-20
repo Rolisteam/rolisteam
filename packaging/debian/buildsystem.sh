cf8701ddc295f2547e1cde77f15e57835d185b31
Renaud G <renaud@rolisteam.org>
2015-05-20 08:20:12 +0200

    Add script to build deb package and source tarball

--- packaging/debian/buildsystem.sh
+++ packaging/debian/buildsystem.sh
@@ -1,46 +1,78 @@
 #!/bin/sh
-VERSION=1.6.1
+
+#############################################
+## Script for submitting rolisteam into ppa
+## There is two ppa: unstable(dev) and stable.
+#############################################
+VERSION=1.7.0
+
+GIT_PROJECT="git@github.com:obiwankennedy/rolisteam.git"
+DEV=1
+
+
+rm -rf rolisteam-$VERSION
+
+function makeTarBall()
+{
+    git clone --recursive $GIT_PROJECT
+    cp ./rolisteam/packaging/rolisteam.desktop ./rolisteam/
+    cp ./rolisteam/packaging/debian/changelog ./rolisteam/
+    #Rename the dir
+    mv ./rolisteam ./rolisteam-$VERSION
+    rm -rf ./rolisteam/packaging
+    find . -name ".git" -exec rm -rf {} \;
+    tar -czf rolisteam-$VERSION.tar.gz rolisteam-$VERSION
+    zip -r rolisteam-$VERSION.zip rolisteam-$VERSION
+}
+
+function build()
+{
+    git clone --recursive $GIT_PROJECT
+    mv rolisteam rolisteam-$VERSION
+    cp ./rolisteam-$VERSION/packaging/rolisteam.desktop ./rolisteam-$VERSION/
+    cp -R ./rolisteam-$VERSION/packaging/debian ./rolisteam-$VERSION/
+    cp ./rolisteam-$VERSION/packaging/debian/changelog ./rolisteam-$VERSION/
+    cd rolisteam-$VERSION
+    id=`head -n 1 changelog | awk '{print $2}' | awk -F ')' '{print $1}' | awk -F 'ubuntu' '{print $2}'`
+    lrelease rolisteam.pro 
+    rm -rf packaging
+    echo "Would you like to create new version in changelog file? [N/y]"
+    read value
+    if [ $a = "y" ]
+    then
+        dch -i
+    fi
+    #dpkg-buildpackage -rfakeroot
+    debuild -S -sa
+    cd ..
+    if [ $DEV -eq 1 ]
+    then 
+        dput -f ppa:rolisteam/rolisteamdev rolisteam_"$VERSION"ubuntu"$id"_source.changes
+    else
+        dput -f ppa:rolisteam/ppa rolisteam_"$VERSION"ubuntu"$id"_source.changes
+    fi
+
+}
+
 
 if [  $# -gt 0 ]
 then
 	for i in "$@"; do
 		if [ "$i" = "clean" ]
 		then
-		    	rm -rf rolisteam
+		   rm -rf rolisteam
 		fi
 		if [ "$i" = "tarball" ]
 		then
-		    mkdir rolisteam
-		    cp ./rolisteam.desktop rolisteam/
-		    cp ./changelog rolisteam/
-		    cd rolisteam
-		    svn export -r406 https://rolisteam.googlecode.com/svn/branches/1.0.0/rolisteam/ rolisteam-$VERSION
-		    cp ./rolisteam.desktop rolisteam-$VERSION/
-		    cp ./changelog rolisteam-$VERSION/
-		    tar -czf rolisteam-$VERSION.tar.gz rolisteam-$VERSION
-		    zip -r rolisteam-$VERSION.zip rolisteam-$VERSION
+		   makeTarBall
 		fi
 		if [ "$i" = "setup" ]
 		then
-		    sudo apt-get install libphonon-dev libqt4-dev build-essential cdbs debhelper wdiff  devscripts dh-make dpatch vim subversion
+		    sudo apt-get install build-essential cdbs debhelper wdiff  devscripts dh-make dpatch vim subversion
 		fi
 		if [ "$i" = "build" ]
 		then
-			svn export -r406 http://rolisteam.googlecode.com/svn/trunk/packaging/rolisteam.desktop rolisteam.desktop
-			mkdir rolisteam
-			cp ./rolisteam.desktop rolisteam/
-			cd rolisteam
-			svn export -r406 https://rolisteam.googlecode.com/svn/branches/1.0.0/rolisteam/ rolisteam-$VERSION
-			mv ./rolisteam.desktop rolisteam-$VERSION
-			cp -R ../debian rolisteam-$VERSION/
-			#find . -name ".svn" -exec rm -rf {} \;  2> /dev/null
-			cd rolisteam-$VERSION
-			cd traduction
-			lrelease rolisteam_*.ts 
-			cd ..
-			dch -i
-			#dpkg-buildpackage -rfakeroot
-			debuild -S -rfakeroot
+			build
 		fi
 	done
 fi
