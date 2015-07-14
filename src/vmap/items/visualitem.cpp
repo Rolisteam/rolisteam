df8bc2745bf13b83a0c78fb5a2addb4c03938d85
Renaud G <renaud@rolisteam.org>
2015-07-14 17:46:52 +0200

    -Add Api to send or receive MoveItem Network message. -Add Api to Id and Map Id.

--- src/vmap/items/visualitem.cpp
+++ src/vmap/items/visualitem.cpp
@@ -18,22 +18,36 @@
     *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
     ***************************************************************************/
 #include "visualitem.h"
+
 #include <QGraphicsSceneHoverEvent>
 #include <math.h>
 #include <QCursor>
 #include <QDebug>
+#include <QUuid>
+
+#include "network/networkmessagewriter.h"
+#include "network/networkmessagereader.h"
 
 VisualItem::VisualItem()
     : QGraphicsObject()
 {
-    setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemSendsGeometryChanges|QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsFocusable);
+    m_id = QUuid::createUuid().toString();
+    init();
+
+
 }
 
 VisualItem::VisualItem(QColor& penColor,QGraphicsItem * parent )
     : QGraphicsObject(parent),m_color(penColor)
 {
-    //m_state = IDLE;
-    setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemSendsGeometryChanges|QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsFocusable);
+   init();
+}
+void VisualItem::init()
+{
+   setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemSendsGeometryChanges|QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsFocusable);
+   connect(this,SIGNAL(xChanged()),this,SLOT(sendPositionMsg()));
+   connect(this,SIGNAL(yChanged()),this,SLOT(sendPositionMsg()));
+   connect(this,SIGNAL(zChanged()),this,SLOT(sendPositionMsg()));
 }
 
 void VisualItem::setPenColor(QColor& penColor)
@@ -55,7 +69,54 @@ void VisualItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
     update();
     QGraphicsItem::mouseReleaseEvent(event);
 }
+void VisualItem::setId(QString id)
+{
+    m_id = id;
+}
 
+QString VisualItem::getId()
+{
+    return m_id;
+}
+
+void VisualItem::sendPositionMsg()
+{
+    NetworkMessageWriter msg(NetMsg::VMapCategory,NetMsg::MoveItem);
+    msg.string8(m_mapId);
+    msg.string16(m_id);
+    msg.real(pos().x());
+    msg.real(pos().y());
+    msg.real(zValue());
+    msg.sendAll();
+}
+void VisualItem::readPositionMsg(NetworkMessageReader* msg)
+{
+    qreal x = msg->real();
+    qreal y = msg->real();
+    qreal z = msg->real();
+
+    setPos(x,y);
+    setZValue(z);
+}
+
+void VisualItem::setMapId(QString id)
+{
+    m_mapId = id;
+}
+
+QString VisualItem::getMapId()
+{
+    return m_mapId;
+}
+
+
+
+
+
+
+
+
+//friend functions
 QDataStream& operator<<(QDataStream& os,const VisualItem& c)
 {
     
