9fe691dbc9841496d1a948f8f178e7a82ac73618
Renaud G <renaud@rolisteam.org>
2015-07-16 16:46:32 +0200

    -Add doxy comments -Add editable mode for visualitems.

--- src/vmap/items/visualitem.cpp
+++ src/vmap/items/visualitem.cpp
@@ -1,22 +1,22 @@
 /***************************************************************************
-    *      Copyright (C) 2010 by Renaud Guezennec                             *
-    *                                                                         *
-    *                                                                         *
-    *   rolisteam is free software; you can redistribute it and/or modify     *
-    *   it under the terms of the GNU General Public License as published by  *
-    *   the Free Software Foundation; either version 2 of the License, or     *
-    *   (at your option) any later version.                                   *
-    *                                                                         *
-    *   This program is distributed in the hope that it will be useful,       *
-    *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-    *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-    *   GNU General Public License for more details.                          *
-    *                                                                         *
-    *   You should have received a copy of the GNU General Public License     *
-    *   along with this program; if not, write to the                         *
-    *   Free Software Foundation, Inc.,                                       *
-    *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
-    ***************************************************************************/
+	*      Copyright (C) 2010 by Renaud Guezennec                             *
+	*                                                                         *
+	*                                                                         *
+	*   rolisteam is free software; you can redistribute it and/or modify     *
+	*   it under the terms of the GNU General Public License as published by  *
+	*   the Free Software Foundation; either version 2 of the License, or     *
+	*   (at your option) any later version.                                   *
+	*                                                                         *
+	*   This program is distributed in the hope that it will be useful,       *
+	*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+	*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+	*   GNU General Public License for more details.                          *
+	*                                                                         *
+	*   You should have received a copy of the GNU General Public License     *
+	*   along with this program; if not, write to the                         *
+	*   Free Software Foundation, Inc.,                                       *
+	*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+	***************************************************************************/
 #include "visualitem.h"
 
 #include <QGraphicsSceneHoverEvent>
@@ -29,84 +29,97 @@
 #include "network/networkmessagereader.h"
 
 VisualItem::VisualItem()
-    : QGraphicsObject()
+	: QGraphicsObject(),m_editable(false)
 {
-    m_id = QUuid::createUuid().toString();
-    init();
+	m_id = QUuid::createUuid().toString();
+	init();
 
 
 }
 
-VisualItem::VisualItem(QColor& penColor,QGraphicsItem * parent )
-    : QGraphicsObject(parent),m_color(penColor)
+VisualItem::VisualItem(QColor& penColor,bool b,QGraphicsItem * parent )
+	: QGraphicsObject(parent),m_color(penColor),m_editable(b)
 {
-   init();
+	init();
 }
 void VisualItem::init()
 {
-   setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemSendsGeometryChanges|QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsFocusable);
-   connect(this,SIGNAL(xChanged()),this,SLOT(sendPositionMsg()));
-   connect(this,SIGNAL(yChanged()),this,SLOT(sendPositionMsg()));
-   connect(this,SIGNAL(zChanged()),this,SLOT(sendPositionMsg()));
+	if(m_editable)
+	{
+		/// @warning if two connected people have editable item, it will lead to endless loop.
+		setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemSendsGeometryChanges|QGraphicsItem::ItemIsMovable|QGraphicsItem::ItemIsFocusable);
+		connect(this,SIGNAL(xChanged()),this,SLOT(sendPositionMsg()));
+		connect(this,SIGNAL(yChanged()),this,SLOT(sendPositionMsg()));
+		connect(this,SIGNAL(zChanged()),this,SLOT(sendPositionMsg()));
+	}
+	else
+	{
+		setFlags(QGraphicsItem::ItemIsSelectable|QGraphicsItem::ItemIsFocusable);
+	}
+}
+void VisualItem::setEditableItem(bool b)
+{
+	m_editable=b;
+	init();
 }
 
 void VisualItem::setPenColor(QColor& penColor)
 {
-    m_color = penColor;
+	m_color = penColor;
 }
 void VisualItem::mouseMoveEvent ( QGraphicsSceneMouseEvent * event )
 {
-    update();
-    QGraphicsItem::mouseMoveEvent(event);
+	update();
+	QGraphicsItem::mouseMoveEvent(event);
 }
 void VisualItem::mousePressEvent ( QGraphicsSceneMouseEvent * event )
 {
-    update();
-    QGraphicsItem::mousePressEvent(event);
+	update();
+	QGraphicsItem::mousePressEvent(event);
 }
 void VisualItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
-    update();
-    QGraphicsItem::mouseReleaseEvent(event);
+	update();
+	QGraphicsItem::mouseReleaseEvent(event);
 }
 void VisualItem::setId(QString id)
 {
-    m_id = id;
+	m_id = id;
 }
 
 QString VisualItem::getId()
 {
-    return m_id;
+	return m_id;
 }
 
 void VisualItem::sendPositionMsg()
 {
-    NetworkMessageWriter msg(NetMsg::VMapCategory,NetMsg::MoveItem);
-    msg.string8(m_mapId);
-    msg.string16(m_id);
-    msg.real(pos().x());
-    msg.real(pos().y());
-    msg.real(zValue());
-    msg.sendAll();
+	NetworkMessageWriter msg(NetMsg::VMapCategory,NetMsg::MoveItem);
+	msg.string8(m_mapId);
+	msg.string16(m_id);
+	msg.real(pos().x());
+	msg.real(pos().y());
+	msg.real(zValue());
+	msg.sendAll();
 }
 void VisualItem::readPositionMsg(NetworkMessageReader* msg)
 {
-    qreal x = msg->real();
-    qreal y = msg->real();
-    qreal z = msg->real();
+	qreal x = msg->real();
+	qreal y = msg->real();
+	qreal z = msg->real();
 
-    setPos(x,y);
-    setZValue(z);
+	setPos(x,y);
+	setZValue(z);
 }
 
 void VisualItem::setMapId(QString id)
 {
-    m_mapId = id;
+	m_mapId = id;
 }
 
 QString VisualItem::getMapId()
 {
-    return m_mapId;
+	return m_mapId;
 }
 
 
@@ -119,14 +132,14 @@ QString VisualItem::getMapId()
 //friend functions
 QDataStream& operator<<(QDataStream& os,const VisualItem& c)
 {
-    
-    c.writeData(os);
-    
-    return os;
+
+	c.writeData(os);
+
+	return os;
 }
 QDataStream& operator>>(QDataStream& is,VisualItem& c)
 {
-    c.readData(is);
-    return is;
+	c.readData(is);
+	return is;
 }
 
