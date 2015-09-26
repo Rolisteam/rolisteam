7daed59a9c3bfe861a5dfc187c2c36d14d4aac1f
Renaud G <renaud@rolisteam.org>
2015-09-26 23:15:45 +0200

    -new item sightitem -first implementation showing disk or conical sight. TODO: must be improve.

--- src/vmap/items/sightitem.cpp
+++ src/vmap/items/sightitem.cpp
@@ -20,8 +20,21 @@
 
 #include "sightitem.h"
 
+
+#include <QDebug>
+#include <QStyleOptionGraphicsItem>
+#include <QMenu>
+#include <QPainter>
+#include <QGraphicsScene>
+
 SightItem::SightItem()
+ : m_shape(DISK),m_angle(120),m_radius(50)
 {
+    setFlag(QGraphicsItem::ItemUsesExtendedStyleOption);
+
+    m_radialGradient = new QRadialGradient();
+    m_conicalGradient = new QConicalGradient();
+    computeGradiants();
 
 }
 
@@ -30,3 +43,123 @@ SightItem::~SightItem()
 
 }
 
+void SightItem::setNewEnd(QPointF& nend)
+{
+
+}
+void SightItem::writeData(QDataStream& out) const
+{
+
+}
+
+void SightItem::readData(QDataStream& in)
+{
+
+}
+VisualItem::ItemType SightItem::getType()
+{
+    return VisualItem::SIGHT;
+}
+
+void SightItem::fillMessage(NetworkMessageWriter* msg)
+{
+
+}
+
+void SightItem::readItem(NetworkMessageReader* msg)
+{
+
+}
+void SightItem::setGeometryPoint(qreal pointId,QPointF& pos)
+{
+
+}
+void SightItem::initChildPointItem()
+{
+
+}
+VisualItem* SightItem::getItemCopy()
+{
+
+}
+void SightItem::paint ( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
+{
+
+    qDebug() << option->rect << option->exposedRect;
+    QBrush brush(*m_gradient);
+
+    painter->setBrush(brush);
+   // painter->setPen(Qt::NoPen);
+
+    painter->drawRect(-boundingRect().width()/2,-boundingRect().height()/2,boundingRect().width(),boundingRect().height());
+
+
+}
+void SightItem::computeGradiants()
+{
+    switch(m_shape)
+    {
+    case DISK:
+        m_radialGradient->setCenter(pos());
+        m_radialGradient->setRadius(m_radius);
+        //m_radialGradient->setFocalPoint(pos());
+        //m_radialGradient->setFocalRadius(m_radius);
+        m_radialGradient->setColorAt(0.0,QColor(0,0,0,0));
+        m_radialGradient->setColorAt(0.89,QColor(0,0,0,0));
+        m_radialGradient->setColorAt(0.9,QColor(0,0,0,120));
+        m_radialGradient->setColorAt(1.0,QColor(0,0,0));
+        m_gradient = m_radialGradient;
+        break;
+    case ANGLE:
+        m_conicalGradient->setCenter(pos());
+        m_conicalGradient->setAngle(0);
+        m_conicalGradient->setColorAt(0.0,QColor(0,0,0,0));
+        m_conicalGradient->setColorAt(0.14,QColor(0,0,0,0));
+        m_conicalGradient->setColorAt(0.15,QColor(0,0,0));
+        m_conicalGradient->setColorAt(0.85,QColor(0,0,0));
+        m_conicalGradient->setColorAt(0.86,QColor(0,0,0,0));
+        m_conicalGradient->setColorAt(1.0,QColor(0,0,0,0));
+        m_gradient = m_conicalGradient;
+        break;
+    }
+}
+QRectF  SightItem::boundingRect() const
+{
+    return scene()->sceneRect();
+}
+void SightItem::setShape(SHAPE shape)
+{
+    m_shape = shape;
+    computeGradiants();
+    update();
+
+}
+void  SightItem::setRaduis(qreal rad)
+{
+    m_radius = rad;
+    computeGradiants();
+    update();
+}
+
+void  SightItem::setAngle(qreal rad)
+{
+    m_angle = rad;
+    computeGradiants();
+    update();
+}
+void SightItem::createActions()
+{
+    m_diskShape = new QAction(tr("Disk Shape"),this);
+    m_diskShape->setCheckable(true);
+    m_angleShape = new QAction(tr("Arc Shape"),this);
+    m_angleShape->setCheckable(true);
+
+    connect(m_diskShape,SIGNAL(triggered()),this,SLOT(closePath()));
+    connect(m_angleShape,SIGNAL(triggered()),this,SLOT(closePath()));
+}
+
+void SightItem::addActionContextMenu(QMenu* menu)
+{
+    menu->addAction(m_diskShape);
+    menu->addAction(m_angleShape);
+}
--- src/vmap/items/sightitem.h
+++ src/vmap/items/sightitem.h
@@ -21,12 +21,74 @@
 #ifndef SIGHTITEM_H
 #define SIGHTITEM_H
 
+#include <QGradient>
+#include <QConicalGradient>
+#include <QRadialGradient>
+#include "visualitem.h"
 
-class SightItem
+class SightItem : public VisualItem
 {
 public:
+    enum SHAPE {DISK,ANGLE};
     SightItem();
-    ~SightItem();
+    virtual ~SightItem();
+
+
+    /**
+     * @brief setNewEnd
+     * @param nend
+     */
+    virtual void setNewEnd(QPointF& nend);
+    /**
+     * @brief writeData
+     * @param out
+     */
+    virtual void writeData(QDataStream& out) const;
+    /**
+     * @brief readData
+     * @param in
+     */
+    virtual void readData(QDataStream& in);
+    virtual VisualItem::ItemType getType();
+    /**
+     * @brief fillMessage
+     * @param msg
+     */
+    virtual void fillMessage(NetworkMessageWriter* msg);
+    /**
+     * @brief readItem
+     * @param msg
+     */
+    virtual void readItem(NetworkMessageReader* msg);
+    virtual void setGeometryPoint(qreal pointId,QPointF& pos) ;
+    virtual void initChildPointItem() ;
+    virtual VisualItem* getItemCopy() ;
+    virtual QRectF boundingRect() const;
+
+    void paint ( QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
+
+    void setShape(SHAPE shape);
+    void setRaduis(qreal rad);
+    void setAngle(qreal rad);
+
+    void createActions();
+    void addActionContextMenu(QMenu* menu);
+
+private slots:
+    void computeGradiants();
+
+private:
+    SHAPE m_shape;
+    qreal m_radius;
+    qreal m_angle;
+
+
+    QAction* m_diskShape;
+    QAction* m_angleShape;
+
+    QGradient* m_gradient;
+    QConicalGradient* m_conicalGradient;
+    QRadialGradient* m_radialGradient;
 };
 
 #endif // SIGHTITEM_H
