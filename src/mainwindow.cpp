3566164ebc37f34a8e3d6d932514f821897d11c1
Renaud G <renaud@rolisteam.org>
2015-10-19 00:13:44 +0200

    remove french comment.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1326,7 +1326,6 @@ void MainWindow::setupUi()
     //readSettings();
     m_preferencesDialog = new PreferencesDialog(this);
     linkActionToMenu();
-    // Creation de l'editeur de notes
     m_noteEditor= new TextEdit(this);
 #ifdef Q_OS_MAC
     m_noteEditor->menuBar()->setNativeMenuBar(false);
