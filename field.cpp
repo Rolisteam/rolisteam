186d85a317baf0c3a50c339ef18ef3d51ad62357
Renaud Guezennec <renaud.guezennec@ext.mpsa.com>
2016-01-05 14:06:07 +0100

    Add field class

---
+++ field.cpp
@@ -0,0 +1,7 @@
+#include "field.h"
+
+Field::Field(QWidget *parent) : QWidget(parent)
+{
+
+}
+
---
+++ field.h
@@ -0,0 +1,17 @@
+#ifndef FIELD_H
+#define FIELD_H
+
+#include <QWidget>
+
+class Field : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Field(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // FIELD_H
