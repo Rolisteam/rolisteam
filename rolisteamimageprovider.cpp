0142b6ce404cb866e20943c4f4be89fff6911624
Renaud Guezennec <renaud.guezennec@ext.mpsa.com>
2016-02-05 16:29:59 +0100

    -Add files for all in rcs

---
+++ rolisteamimageprovider.cpp
@@ -0,0 +1,23 @@
+#include "rolisteamimageprovider.h"
+
+RolisteamImageProvider::RolisteamImageProvider()
+    :  QQuickImageProvider(QQuickImageProvider::Pixmap)
+{
+
+}
+
+QPixmap RolisteamImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize, bool requestedAutoTransform)
+{
+    QPixmap pixmap= m_data.value(id);
+
+    if (NULL!=size)
+        return pixmap.copy(0,0,size->width(),size->height());
+    else
+        return pixmap;
+}
+
+void RolisteamImageProvider::insertPix(QString key, QPixmap img)
+{
+    m_data[key]=img;
+}
+
