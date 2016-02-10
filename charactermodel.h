5dfd84d377487e2f4dc0bfb200d703159cf29b0b
Renaud G <renaud@rolisteam.org>
2016-02-10 17:12:43 +0100

    blank implementation for charactermodel

--- charactermodel.h
+++ charactermodel.h
@@ -1,11 +1,41 @@
 #ifndef CHARACTERMODEL_H
 #define CHARACTERMODEL_H
 
+#include "fieldmodel.h"
 
+#include <QAbstractItemModel>
+/**
+ * @brief The FieldModel class
+ */
 class CharacterModel : public QAbstractItemModel
 {
+    Q_OBJECT
 public:
-    CharacterModel();
+    explicit CharacterModel(QObject *parent = 0);
+
+    QVariant data(const QModelIndex &index, int role) const;
+    QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role);
+    void appendField(Field* f);
+
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    void save(QJsonObject& json,bool exp=false);
+    void load(QJsonObject& json,QGraphicsScene* scene);
+
+    void generateQML(QTextStream& out,Item::QMLSection sec);
+
+    Q_INVOKABLE QString getValue(const QString& key);
+
+
+
+private:
+    Section* m_rootSection;
+
 };
+
 
 #endif // CHARACTERMODEL_H
