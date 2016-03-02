ee3852d2e82fd9ed271cf8cae99abc984d849afa
Renaud G <renaud@rolisteam.org>
2016-03-02 09:28:10 +0100

    prevent segfault

--- section.cpp
+++ section.cpp
@@ -139,12 +139,15 @@ void Section::load(QJsonObject &json,QList<QGraphicsScene*> scenes)
             item = field;
             gItem = field;
         }
-        QGraphicsScene* scene = scenes.at(item->getPage());
-        if((NULL!=scene)&&(NULL!=gItem))
+        if(scenes.size()>item->getPage())
         {
-            scene->addItem(gItem);
+            QGraphicsScene* scene = scenes.at(item->getPage());
+            if((NULL!=scene)&&(NULL!=gItem))
+            {
+                scene->addItem(gItem);
+            }
+            item->load(obj,scenes);
         }
-        item->load(obj,scenes);
         item->setParent(this);
         m_dataHash.insert(item->getPath(),item);
         m_keyList.append(item->getPath());
