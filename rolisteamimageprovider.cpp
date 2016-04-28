00c4a742fdad00c980bfc6d7df76eed3e35191e6
Renaud G <renaud@rolisteam.org>
2016-04-29 00:15:12 +0200

    -Add API to serialize the image provider into networkmessage (Not working).

--- rolisteamimageprovider.cpp
+++ rolisteamimageprovider.cpp
@@ -37,3 +37,31 @@ void RolisteamImageProvider::setData(const QHash<QString, QPixmap> &data)
     m_data = data;
 }
 
+void RolisteamImageProvider::fill(NetworkMessageWriter &msg)
+{
+    msg.uint16(m_data.count());
+    for(auto key : m_data.keys())
+    {
+        msg.string16(key);
+
+        QByteArray array;
+        QDataStream in(array);
+        in << m_data[key];
+
+        msg.byteArray32(array);
+    }
+}
+
+void RolisteamImageProvider::read(NetworkMessageReader &msg)
+{
+    int size = msg.uint16();
+    for(int i = 0; i<size; ++i)
+    {
+        QString key = msg.string16();
+        QByteArray array = msg.byteArray32();
+        QPixmap pix;
+        pix.loadFromData(array);
+        m_data.insert(key,pix);
+    }
+}
+
