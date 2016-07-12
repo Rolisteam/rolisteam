6eb530079493e0e19768a61176a14ab551164be7
Renaud G <renaud@rolisteam.org>
2016-07-12 19:29:04 +0200

    first implementation for heartBeat management. To be initialized into code and tested.

--- src/network/heartbeatsender.h
+++ src/network/heartbeatsender.h
@@ -2,16 +2,32 @@
 #define HEARTBEATSENDER_H
 
 #include <QObject>
+#include <QTimer>
 
-class heartBeatSender : public QObject
+#include "preferences/preferenceslistener.h"
+#include "preferences/preferencesmanager.h"
+/**
+ * @brief The heartBeatSender class is dedicated to send off heartbeat messages preventing
+ * disconnection for some users.
+ */
+class heartBeatSender : public QObject, public PreferencesListener
 {
     Q_OBJECT
 public:
     explicit heartBeatSender(QObject *parent = 0);
 
-signals:
+    void preferencesHasChanged(QString);
+    void updateStatus();
 
 public slots:
+
+    void sendHeartBeatMsg();
+    void updateTimer();
+private:
+    QTimer m_timer;
+    PreferencesManager* m_preferences;
+    int m_timeOut;
+    bool m_status;
 };
 
 #endif // HEARTBEATSENDER_H
