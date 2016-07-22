11680e65aac84aedd4ac47204c749baf659611d1
Renaud G <renaud@rolisteam.org>
2016-07-22 17:26:56 +0200

    add paranthese node.

---
+++ formula/nodes/parenthesesfnode.cpp
@@ -0,0 +1,6 @@
+#include "parenthesesfnode.h"
+
+ParenthesesFNode::ParenthesesFNode()
+{
+
+}
---
+++ formula/nodes/parenthesesfnode.h
@@ -0,0 +1,11 @@
+#ifndef PARENTHESESFNODE_H
+#define PARENTHESESFNODE_H
+
+
+class ParenthesesFNode
+{
+public:
+    ParenthesesFNode();
+};
+
+#endif // PARENTHESESFNODE_H
