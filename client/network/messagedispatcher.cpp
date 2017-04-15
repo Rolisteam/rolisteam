8e3072882ec1897d30f7ca3e594b241a71321a01
Renaud G <renaud@rolisteam.org>
2017-04-16 00:54:33 +0200

    -Implemention a messageDispatcher.

--- client/network/messagedispatcher.cpp
+++ client/network/messagedispatcher.cpp
@@ -1,6 +1,27 @@
 #include "messagedispatcher.h"
+#include "network/networkmessagereader.h"
+
 
 MessageDispatcher::MessageDispatcher(QObject *parent) : QObject(parent)
 {
+
+}
+
+void MessageDispatcher::dispatchMessage(QByteArray data, Channel* channel, TcpClient *emitter)
+{
+    NetworkMessageReader* msg = new NetworkMessageReader();
+
+    msg->setData(data);
+
+    switch (msg->category())
+    {
+            case NetMsg::AdministrationCategory:
+                emit messageForAdmin(msg);
+            break;
+            default:
+                channel->sendToAll(msg,emitter);
+            break;
+    }
+
 
 }
--- client/network/messagedispatcher.h
+++ client/network/messagedispatcher.h
@@ -2,14 +2,29 @@
 #define MESSAGEDISPATCHER_H
 
 #include <QObject>
+#include <QByteArray>
+#include "channelmodel.h"
+#include "tcpclient.h"
+#include "networkmessage.h"
 
+
+class NetworkLink;
+class NetworkMessageReader;
+/**
+ * @brief The MessageDispatcher class
+ */
 class MessageDispatcher : public QObject
 {
     Q_OBJECT
 public:
     explicit MessageDispatcher(QObject *parent = 0);
 
+
+    void dispatchMessage(QByteArray msg, Channel* channel, TcpClient* emitter);
+
 signals:
+    void messageForAdmin(NetworkMessageReader* );
+
 
 public slots:
 };
