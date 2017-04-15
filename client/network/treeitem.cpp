3d8904ab942a00b5fa5242b386b66fb8e34e24d6
Renaud G <renaud@rolisteam.org>
2017-04-16 00:59:00 +0200

    -create file for this class.

--- client/network/treeitem.cpp
+++ client/network/treeitem.cpp
@@ -4,3 +4,53 @@ TreeItem::TreeItem()
 {
 
 }
+
+void TreeItem::addChild()
+{
+
+}
+
+bool TreeItem::isLeaf() const
+{
+    return true;
+}
+
+int TreeItem::childCount() const
+{
+    return 0;
+}
+
+int TreeItem::addChild(TreeItem *)
+{
+    return 0;
+}
+
+TreeItem *TreeItem::getChildAt(int row)
+{
+    return nullptr;
+}
+
+TreeItem *TreeItem::getParent() const
+{
+    return m_parent;
+}
+
+void TreeItem::setParent(TreeItem *parent)
+{
+    m_parent = parent;
+}
+
+QString TreeItem::getName() const
+{
+    return m_name;
+}
+
+void TreeItem::setName(const QString &name)
+{
+    m_name = name;
+}
+
+int TreeItem::rowInParent()
+{
+    return m_parent->indexOf(this);
+}
--- client/network/treeitem.h
+++ client/network/treeitem.h
@@ -1,11 +1,33 @@
 #ifndef TREEITEM_H
 #define TREEITEM_H
 
-
+#include <QString>
+/**
+ * @brief The TreeItem class
+ */
 class TreeItem
 {
 public:
     TreeItem();
+
+    virtual void addChild();
+    virtual bool isLeaf() const;
+    virtual int childCount() const;
+    virtual int addChild(TreeItem*);
+    TreeItem* getChildAt(int row);
+
+    TreeItem* getParent() const;
+    void setParent(TreeItem* parent);
+
+    QString getName() const;
+    void setName(const QString &name);
+
+    virtual int indexOf(TreeItem*) = 0;
+    int rowInParent();
+
+protected:
+    QString m_name;
+    TreeItem* m_parent;
 };
 
 #endif // TREEITEM_H
