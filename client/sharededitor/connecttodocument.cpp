ecac404a9b336e144ab7eb169e51ec265ee97573
rguezennec <renaud@rolisteam.org>
2017-07-17 17:36:16 +0200

    -remove some files.

--- client/sharededitor/connecttodocument.cpp
+++ client/sharededitor/connecttodocument.cpp
@@ -17,7 +17,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "connecttodocument.h"
-#include "ui_connecttodocument.h"
 
 #include <QDebug>
 #include <QSettings>
--- client/sharededitor/firstrundialog.cpp
+++ client/sharededitor/firstrundialog.cpp
@@ -17,7 +17,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "firstrundialog.h"
-#include "ui_firstrundialog.h"
+//#include "ui_firstrundialog.h"
 
 #include "utilities.h"
 
