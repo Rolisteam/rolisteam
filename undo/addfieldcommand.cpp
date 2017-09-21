0d5c4c9b90016f7799d46636be89ab8ef292ed54
renaud.guezennec <renaud.guezennec@softbankrobotics.com>
2017-09-21 12:58:24 +0200

    adding add field command

--- undo/addfieldcommand.cpp
+++ undo/addfieldcommand.cpp
@@ -1,6 +1,78 @@
 #include "addfieldcommand.h"
+#include "field.h"
+#include "tablecanvasfield.h"
 
-AddFieldCommand::AddFieldCommand()
+AddFieldCommand::AddFieldCommand(Canvas::Tool tool, Canvas* canvas,FieldModel* model,int currentPage,QPointF pos,QUndoCommand *parent)
+  : QUndoCommand(parent),m_canvas(canvas),m_model(model),m_currentPage(currentPage)
 {
 
+  m_field = new Field(pos);
+  if(Canvas::ADDTABLE == tool)
+  {
+      m_field->setCanvasField(new TableCanvasField(m_field));
+  }
+
+  m_canvas->addItem(m_field->getCanvasField());
+  if(nullptr != m_model)
+  {
+      m_model->appendField(m_field);
+  }
+  m_field->setValueFrom(CharacterSheetItem::X,pos.x());
+  m_field->setValueFrom(CharacterSheetItem::Y,pos.y());
+
+  //m_currentItem->setFocus();
+  switch(tool)//TEXTINPUT,TEXTFIELD,TEXTAREA,SELECT,CHECKBOX,IMAGE,BUTTON
+  {
+  case Canvas::ADDCHECKBOX:
+      m_field->setCurrentType(Field::CHECKBOX);
+      break;
+  case Canvas::ADDINPUT:
+      m_field->setCurrentType(Field::TEXTINPUT);
+      break;
+  case Canvas::ADDTEXTAREA:
+      m_field->setCurrentType(Field::TEXTAREA);
+      break;
+  case Canvas::ADDTEXTFIELD:
+      m_field->setCurrentType(Field::TEXTFIELD);
+      break;
+  case Canvas::ADDTABLE:
+  {
+      m_field->setCurrentType(Field::TABLE);
+  }
+      break;
+  case Canvas::ADDIMAGE:
+      m_field->setCurrentType(Field::IMAGE);
+      break;
+  case Canvas::ADDFUNCBUTTON:
+      m_field->setCurrentType(Field::FUNCBUTTON);
+      break;
+  case Canvas::BUTTON:
+      m_field->setCurrentType(Field::BUTTON);
+      m_field->setBgColor(Qt::red);
+      break;
+  case Canvas::MOVE:
+  case Canvas::DELETETOOL:
+  case Canvas::NONE:
+      break;
+  }
+}
+
+void AddFieldCommand::undo()
+{
+  m_canvas->removeItem(m_field->getCanvasField());
+  m_canvas->update();
+  m_model->removeField(m_field);
+
+}
+
+void AddFieldCommand::redo()
+{
+  m_canvas->addItem(m_field->getCanvasField());
+  m_model->appendField(m_field);
+
+}
+
+Field *AddFieldCommand::getField() const
+{
+  return m_field;
 }
--- undo/addfieldcommand.h
+++ undo/addfieldcommand.h
@@ -1,11 +1,26 @@
 #ifndef ADDFIELDCOMMAND_H
 #define ADDFIELDCOMMAND_H
 
+#include <QUndoCommand>
+#include "canvas.h"
 
 class AddFieldCommand : public QUndoCommand
 {
 public:
-  AddFieldCommand();
+  AddFieldCommand(Canvas::Tool tool, Canvas* graphicsScene,FieldModel* model,int currentPage,QPointF pos,
+                  QUndoCommand *parent = 0);
+
+  void undo() override;
+  void redo() override;
+
+  Field* getField() const;
+
+private:
+  Field* m_field;
+  Canvas* m_canvas;
+  QPointF initialPosition;
+  FieldModel* m_model;
+  int m_currentPage;
 };
 
 #endif // ADDFIELDCOMMAND_H
