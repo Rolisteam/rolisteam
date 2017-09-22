bcc0077253864e2a871c32d927f4f7556cd27d05
Renaud G <renaud@rolisteam.org>
2017-09-22 12:08:56 +0200

    -Implementation of delete

--- undo/deletefieldcommand.cpp
+++ undo/deletefieldcommand.cpp
@@ -1,6 +1,27 @@
 #include "deletefieldcommand.h"
 
-DeleteFieldCommand::DeleteFieldCommand()
+DeleteFieldCommand::DeleteFieldCommand(Field* field, Canvas* graphicsScene,FieldModel* model,int currentPage,QUndoCommand *parent)
+    : QUndoCommand(parent),m_field(field),m_canvas(graphicsScene),m_model(model),m_currentPage(currentPage)
 {
+    if(nullptr != m_field->getCanvasField())
+    {
+       m_parent = m_field->getParent();
+       m_point = m_field->getCanvasField()->pos();
+       m_posInModel = m_parent->indexOfChild(m_field);
 
+       setText(QObject::tr("Delete %1 %2 Field").arg(field->getId()).arg(field->getLabel()));
+    }
+}
+
+void DeleteFieldCommand::undo()
+{
+    m_canvas->addItem(m_field->getCanvasField());
+    m_field->getCanvasField()->setPos(m_point);
+    m_model->insertField(m_field,m_parent,m_posInModel);
+}
+
+void DeleteFieldCommand::redo()
+{
+    m_canvas->removeItem(m_field->getCanvasField());
+    m_model->removeField(m_field);
 }
--- undo/deletefieldcommand.h
+++ undo/deletefieldcommand.h
@@ -1,11 +1,25 @@
 #ifndef DELETEFIELDCOMMAND_H
 #define DELETEFIELDCOMMAND_H
 
+#include <QUndoCommand>
+#include "canvas.h"
 
-class DeleteFieldCommand
+class DeleteFieldCommand : public QUndoCommand
 {
 public:
-  DeleteFieldCommand();
+  DeleteFieldCommand(Field* field, Canvas* graphicsScene, FieldModel* model,int currentPage,QUndoCommand *parent = 0);
+
+  void undo() override;
+  void redo() override;
+
+private:
+  Field* m_field;
+  Canvas* m_canvas;
+  QPointF m_point;
+  FieldModel* m_model;
+  int m_currentPage;
+  int m_posInModel;
+  CharacterSheetItem* m_parent;
 };
 
 #endif // DELETEFIELDCOMMAND_H
