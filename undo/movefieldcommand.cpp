40104172df150811453813c50f4b1ba9058e8116
Renaud G <renaud@rolisteam.org>
2017-09-22 12:09:09 +0200

    -Implementation of move command

--- undo/movefieldcommand.cpp
+++ undo/movefieldcommand.cpp
@@ -1,6 +1,34 @@
 #include "movefieldcommand.h"
 
-MoveFieldCommand::MoveFieldCommand()
+MoveFieldCommand::MoveFieldCommand(QList<QGraphicsItem*> list, QList<QPointF> oldPos, QUndoCommand* parent)
+    : QUndoCommand(parent),m_list(list),m_oldPoints(oldPos)
 {
+    if(m_list.size() == m_oldPoints.size())
+    {
+        for(auto item : m_list)
+        {
+            m_newPoints.append(item->pos());
+        }
+    }
+    setText(QObject::tr("Move %n Field(s)","",m_list.size()));
+}
 
+void MoveFieldCommand::undo()
+{
+    int i = 0;
+    for(auto item : m_list)
+    {
+        item->setPos(m_oldPoints.at(i));
+        ++i;
+    }
+}
+
+void MoveFieldCommand::redo()
+{
+    int i = 0;
+    for(auto item : m_list)
+    {
+        item->setPos(m_newPoints.at(i));
+        ++i;
+    }
 }
--- undo/movefieldcommand.h
+++ undo/movefieldcommand.h
@@ -1,11 +1,23 @@
 #ifndef MOVEFIELDCOMMAND_H
 #define MOVEFIELDCOMMAND_H
 
+#include <QUndoCommand>
+#include <QList>
+#include <QGraphicsItem>
 
-class MoveFieldCommand
+class MoveFieldCommand : public QUndoCommand
 {
 public:
-  MoveFieldCommand();
+  MoveFieldCommand(QList<QGraphicsItem*> list, QList<QPointF> oldPos, QUndoCommand* parent = 0);
+
+  void undo() override;
+  void redo() override;
+
+private:
+  QList<QGraphicsItem*> m_list;
+  QList<QPointF> m_oldPoints;
+  QList<QPointF> m_newPoints;
+
 };
 
 #endif // MOVEFIELDCOMMAND_H
