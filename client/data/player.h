4c9bcb9485a1031b76c4a45591b8933646ac3d22
Renaud G <renaud@rolisteam.org>
2017-10-11 11:58:47 +0200

    add new line

--- client/data/player.h
+++ client/data/player.h
@@ -162,6 +162,7 @@ public:
     virtual QHash<QString,QString> getVariableDictionnary();
 
     virtual bool isLeaf() const;
+
 private:
     friend class PlayersList;
     friend class SendFeaturesIterator;
