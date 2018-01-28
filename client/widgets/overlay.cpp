88f95cdf1f6fc841c71c43e9b77f032c4608d9ea
Renaud G <renaud@rolisteam.org>
2018-01-28 02:27:31 +0100

    -Add overlay widget.

---
+++ client/widgets/overlay.cpp
@@ -0,0 +1,130 @@
+#include "overlay.h"
+
+#include <QPainter>
+#include <QPen>
+#include <QPainterPath>
+#include <QMouseEvent>
+#include <QDebug>
+
+#define DISTANCE_SELECTION 20
+#define GREY_COLOR_LEVEL 60
+
+
+Overlay::Overlay(QRect rect, QWidget * parent)
+: QWidget(parent),m_rect(rect),m_fullRect(rect)
+{
+    auto y = m_rect.y();
+    m_rect.setY(0);
+    m_rect.setHeight(m_rect.height()-y);
+   // qDebug() << "rect" << m_rect;
+
+    setAttribute(Qt::WA_NoSystemBackground);
+    //setAttribute(Qt::WA_TransparentForMouseEvents);
+}
+void Overlay::paintEvent(QPaintEvent *)
+{
+        QPainter painter(this);
+
+        QPainterPath toShow;
+        toShow.addRect(m_fullRect);
+
+        QPainterPath toHide;
+        toHide.addRect(m_rect);
+        toShow=toShow.subtracted(toHide);
+
+        painter.fillPath(toShow,QColor(GREY_COLOR_LEVEL,GREY_COLOR_LEVEL,GREY_COLOR_LEVEL,100));
+        auto pen = painter.pen();
+        pen.setWidth(4);
+        pen.setStyle(Qt::DashDotLine);
+        pen.setColor(Qt::red);
+        painter.setPen(pen);
+
+        painter.drawRect(m_rect);
+        auto offset = DISTANCE_SELECTION/2;
+        painter.fillRect(m_rect.topLeft().x()-offset,m_rect.topLeft().y()-offset,DISTANCE_SELECTION,DISTANCE_SELECTION,Qt::red);
+        painter.fillRect(m_rect.topRight().x()-offset,m_rect.topRight().y()-offset,DISTANCE_SELECTION,DISTANCE_SELECTION,Qt::red);
+        painter.fillRect(m_rect.bottomLeft().x()-offset,m_rect.bottomLeft().y()-offset,DISTANCE_SELECTION,DISTANCE_SELECTION,Qt::red);
+        painter.fillRect(m_rect.bottomRight().x()-offset,m_rect.bottomRight().y()-offset,DISTANCE_SELECTION,DISTANCE_SELECTION,Qt::red);
+}
+
+QRect Overlay::rect() const
+{
+    return m_rect;
+}
+
+void Overlay::setRect(const QRect &rect)
+{
+    m_rect = rect;
+    update();
+}
+int Overlay::computeDistance(QPoint p, QPoint r)
+{
+  auto result = p-r;
+  //qDebug() << result.manhattanLength();
+  return result.manhattanLength();
+}
+void Overlay::mousePressEvent(QMouseEvent* event)
+{
+   // qDebug() << "mouse press"<<event->pos() << event->globalPos();
+    auto pos = event->pos();
+    if(computeDistance(pos,m_rect.topLeft())<DISTANCE_SELECTION)
+    {
+      m_currentCorner = First;
+    }
+    else if(computeDistance(pos,m_rect.topRight())<DISTANCE_SELECTION)
+    {
+      m_currentCorner = Second;
+    }
+    else if(computeDistance(pos,m_rect.bottomRight())<DISTANCE_SELECTION)
+    {
+      m_currentCorner = Third;
+    }
+    else if(computeDistance(pos,m_rect.bottomLeft())<DISTANCE_SELECTION)
+    {
+      m_currentCorner = Fourth;
+    }
+    else if(computeDistance(pos,m_rect.center())<DISTANCE_SELECTION)
+    {
+      m_currentCorner = Center;
+    }
+    else
+    {
+      m_currentCorner = None;
+    }
+ //   qDebug() << "corner" <<m_currentCorner;
+
+    if(m_currentCorner == None)
+      QWidget::mousePressEvent(event);
+    else
+      m_lastPosition = pos;
+}
+
+void Overlay::mouseMoveEvent(QMouseEvent* event)
+{
+ //   qDebug() << "mouse move"<<event->pos() << event->globalPos();
+  auto pos = event->pos();
+  auto deplace = pos-m_lastPosition;
+
+    switch(m_currentCorner)
+    {
+      case First:
+      m_rect.setTopLeft(pos);
+      break;
+    case Second:
+      m_rect.setTopRight(pos);
+      break;
+    case Third:
+      m_rect.setBottomRight(pos);
+      break;
+    case Fourth:
+      m_rect.setBottomLeft(pos);
+      break;
+    case Center:
+      m_rect.translate(deplace);
+      break;
+    default:
+      break;
+    }
+    m_lastPosition = event->pos();
+    update();
+}
---
+++ client/widgets/overlay.h
@@ -0,0 +1,31 @@
+#ifndef OVERLAY_H
+#define OVERLAY_H
+
+#include <QWidget>
+#include <QRect>
+
+
+class Overlay : public QWidget
+{
+public:
+    enum Position {None, First, Second,Third,Fourth,Center};//follow clock,
+    Overlay(QRect rect, QWidget * parent = nullptr);
+
+    QRect rect() const;
+    void setRect(const QRect &rect);
+
+protected:
+    void paintEvent(QPaintEvent *) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+private:
+    int computeDistance(QPoint p, QPoint r);
+private:
+    QRect m_rect;
+    QRect m_fullRect;
+    Position m_currentCorner;
+    QPoint m_lastPosition;
+};
+
+#endif // OVERLAY_H
