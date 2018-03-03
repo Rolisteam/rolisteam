4cf19a9d3caf1149165db586f672d8fbb9f770da
Renaud G <renaud@rolisteam.org>
2018-03-03 22:23:03 +0100

    log panel

--- widgets/logpanel.h
+++ widgets/logpanel.h
@@ -12,11 +12,22 @@ class LogPanel : public QWidget
     Q_OBJECT
 
 public:
-    explicit LogPanel(QWidget *parent = 0);
+    enum Level {
+        Error,
+        Debug,
+        Warning,
+        Info
+    };
+    explicit LogPanel(QWidget* parent = 0);
     ~LogPanel();
 
+public slots:
+    void appendMessage(QString msg, Level lvl);
+protected slots:
+    void saveLog();
 private:
     Ui::LogPanel *ui;
+    Level m_currentLevel = LogPanel::Error;
 };
 
 #endif // LOGPANEL_H
