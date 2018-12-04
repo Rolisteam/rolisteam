2396bb6c777f38695cca32a7430d0572f64a4ea6
Renaud G <renaud@rolisteam.org>
2018-12-04 14:19:05 +0100

    improve runTest script

--- tests/runTest.sh
+++ tests/runTest.sh
@@ -1,25 +1,18 @@
 #!/bin/sh
-#https://developer.github.com/guides/using-ssh-agent-forwarding/
-#source ~/.bash_profile
-source ~/.ssh/environment
-#source ~/.bashrc
 
 
-QT_DIR="/home/renaud/application/other/Qt/5.9/gcc_64/bin/"
+export DISPLAY=:1
 
-export DISPLAY=:0
+echo "Starting test compilation"
+DEST=test_directory
 
-cd /home/renaud/application/mine/UnitTestRolisteam/lib/
-git clone --recursive https://github.com/Rolisteam/rolisteam.git
-git clone https://github.com/Rolisteam/DiceParser.git
+rm -rf $DEST
+mkdir -p $DEST
+cd $DEST
+qmake -r ../tests.pro
+make -j8 > /dev/null
 
-rm -rf /home/renaud/application/mine/build-UnitTestRolisteam-Debug/*
-mkdir -p /home/renaud/application/mine/build-UnitTestRolisteam-Debug/
-cd /home/renaud/application/mine/build-UnitTestRolisteam-Debug
-$QT_DIR/qmake -r /home/renaud/application/mine/UnitTestRolisteam/UnitTestRolisteam.pro
-make -j 8 > /dev/null
+find . -executable -type f -exec bash -c {} \;
 
-find /home/renaud/application/mine/build-UnitTestRolisteam-Debug -executable -type f -exec bash -c {} \;
-
-cd /home/renaud/application/mine/rolisteam/
-doxygen rolisteam.dox  > /dev/null 2>&1
+echo "end of test"
+#doxygen rolisteam.dox  > /dev/null 2>&1
