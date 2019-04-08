75636b2daaf6d19d4c27f8585b5fdd63bb3dcbf6
Renaud G <renaud@rolisteam.org>
2019-04-08 02:15:23 +0200

    add italian for server

---
+++ translations/roliserver_it.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ChannelModel</name>
+    <message>
+        <location filename="../client/network/channelmodel.cpp" line="397" />
+        <source>Channel Password</source>
+        <translation>La Password Del Canale</translation>
+    </message>
+    <message>
+        <location filename="../client/network/channelmodel.cpp" line="398" />
+        <source>Channel %1 required password:</source>
+        <translation>Canale %1 richiesta password:</translation>
+    </message>
+</context>
+<context>
+    <name>Character</name>
+    <message>
+        <location filename="../client/data/character.cpp" line="596" />
+        <source>%1:
+HP: %2/%3
+State: %4
+Initiative Score: %5
+Distance Per Turn: %6
+type: %7
+</source>
+        <translation>%1:
+HP: %2/%3
+Stato: %4
+Iniziativa Punteggio: %5
+Passi Per Giro: %6
+tipo: %7
+</translation>
+    </message>
+    <message>
+        <location filename="../client/data/character.cpp" line="602" />
+        <source>Not defined</source>
+        <translation>Non definito</translation>
+    </message>
+    <message>
+        <location filename="../client/data/character.cpp" line="612" />
+        <source>NPC</source>
+        <translation>NPC</translation>
+    </message>
+    <message>
+        <location filename="../client/data/character.cpp" line="612" />
+        <source>PC</source>
+        <translation>PC</translation>
+    </message>
+    <message>
+        <location filename="../client/data/character.cpp" line="755" />
+        <source>Initiative</source>
+        <translation>Iniziativa</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../server/main.cpp" line="94" />
+        <source>Unknown</source>
+        <translation>Sconosciuto</translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="113" />
+        <source>Set the path to configuration file [mandatory]</source>
+        <translation>Impostare il percorso per il file di configurazione [obbligatorio]</translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="116" />
+        <source>Print a default configuration file into Standard output</source>
+        <translation>Stampa di un file di configurazione di default in uscita Standard</translation>
+    </message>
+    <message>
+        <location filename="../server/main.cpp" line="119" />
+        <source>Ask for password and return its hash key.</source>
+        <translation>Chiedere la password e restituire la chiave hash.</translation>
+    </message>
+</context>
+<context>
+    <name>ServerManager</name>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="92" />
+        <source>Rolisteam Server is on!</source>
+        <translation>Rolisteam Server!</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="100" />
+        <source>Retry start server in %1s!</source>
+        <translation>Tentativi di avviare il server in %1s!</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="106" />
+        <source>Retry count reached. Server stops trying.</source>
+        <translation>Numero di tentativi raggiunto. Server smette di cercare.</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="271" />
+        <source>Authentification as Admin fails: %2 - %1, Wrong password.</source>
+        <translation>Autenticazione come Amministratore si verifica un errore: %2 - %1, password Errata.</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="297" />
+        <source>Authentification fails: %1 try to connect to the server with wrong password.</source>
+        <translation>Autenticazione ha esito negativo: %1 tenta di connettersi al server con una password errata.</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="316" />
+        <source>User has been kick out: %2 - %1.</source>
+        <translation>L'utente &#232; stato kick out: %2 - %1.</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="470" />
+        <source>New Incoming Connection!</source>
+        <translation>Nuova Connessione In Ingresso!</translation>
+    </message>
+    <message>
+        <location filename="../client/network/servermanager.cpp" line="510" />
+        <source>Disconnected!</source>
+        <translation>Scollegato!</translation>
+    </message>
+</context>
+</TS>
