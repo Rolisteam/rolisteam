743b098e0cab77f13f2311fb29e2282413826a99
Renaud G <renaud@rolisteam.org>
2020-02-23 01:21:52 +0100

    [core] reduce mainwindow role.

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -73,6 +73,7 @@
 #include "widgets/tipofdayviewer.h"
 #include "widgets/toolsbar.h"
 #include "widgets/workspace.h"
+
 #include "worker/messagehelper.h"
 #include "worker/playermessagehelper.h"
 
@@ -1546,9 +1547,6 @@ void MainWindow::cleanUpData()
 
 void MainWindow::postConnection()
 {
-
-    MessageHelper::sendOffPlayerInformations(m_gameController->playerController()->localPlayer());
-
     if(nullptr != m_preferences)
     {
         m_preferences->registerValue("isClient", true);
