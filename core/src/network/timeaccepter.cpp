9704bef74ca052921cedac9ed9e8ca3300ff5926
Renaud G <renaud@rolisteam.org>
2020-05-31 00:03:44 +0200

    [network] fix time accepter.

--- core/src/network/timeaccepter.cpp
+++ core/src/network/timeaccepter.cpp
@@ -7,24 +7,30 @@ TimeAccepter::TimeAccepter() {}
 
 bool TimeAccepter::isValid(const QMap<QString, QVariant>& data) const
 {
-    QTime time(QTime::currentTime());
+    auto startStr= data["TimeStart"].toString();
+    auto endStr= data["TimeEnd"].toString();
+
+    if(startStr.isEmpty() || endStr.isEmpty())
+        return true;
+
+    QDateTime time= QDateTime::currentDateTime();
     const QString format= QStringLiteral("hh:mm");
 
-    QTime start= QTime::fromString(data["TimeStart"].toString(), format);
-    QTime end= QTime::fromString(data["TimeEnd"].toString(), format);
+    QTime start= QTime::fromString(startStr, format);
+    QTime end= QTime::fromString(endStr, format);
     QDateTime dateStart;
+    dateStart.setDate(time.date());
     dateStart.setTime(start);
     QDateTime dateEnd;
+    dateEnd.setDate(time.date());
     dateEnd.setTime(end);
-
-    QDateTime dateTime;
-    dateTime.setTime(time);
 
     if(dateEnd < dateStart)
         dateEnd= dateEnd.addDays(1);
 
     bool result= true;
-    if(start.isValid() && end.isValid() && ((dateTime < dateStart) || (dateTime > dateEnd)))
+
+    if(dateStart.isValid() && dateEnd.isValid() && ((time < dateStart) || (time > dateEnd)))
     {
         result= false;
         qInfo() << QStringLiteral("Connection out of time slot. Connection refused");
