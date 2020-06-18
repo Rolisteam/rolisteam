7b0588bb7c2d8bac3a9e9520aabb363cccc640ec
Renaud G <renaud@rolisteam.org>
2020-06-18 12:06:48 +0200

    [core] new file for the instant messaging.

---
+++ core/src/qmlchat/filterinstantmessagingmodel.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *	Copyright (C) 2020 by Renaud Guezennec                               *
+ *   http://www.rolisteam.org/contact                                      *
+ *                                                                         *
+ *   This software is free software; you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "filterinstantmessagingmodel.h"
+
+FilterInstantMessagingModel::FilterInstantMessagingModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+}
+
+int FilterInstantMessagingModel::rowCount(const QModelIndex &parent) const
+{
+    // For list models only the root node (an invalid parent) should return the list's size. For all
+    // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
+    if (parent.isValid())
+        return 0;
+
+    // FIXME: Implement me!
+}
+
+QVariant FilterInstantMessagingModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    // FIXME: Implement me!
+    return QVariant();
+}
