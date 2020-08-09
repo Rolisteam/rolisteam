57012bb2edb05c9ae8110a5284561fe4d64a6eb7
Renaud G <renaud@rolisteam.org>
2020-08-09 17:33:31 +0200

    [core] improve template code.

--- core/src/updater/media/mediaupdaterinterface.cpp
+++ core/src/updater/media/mediaupdaterinterface.cpp
@@ -21,39 +21,9 @@
 
 #include "controller/media_controller/mediamanagerbase.h"
 
-#include "network/networkmessagewriter.h"
-#include "worker/convertionhelper.h"
+MediaUpdaterInterface::MediaUpdaterInterface(QObject* object) : QObject(object) {}
 
-MediaUpdaterInterface::MediaUpdaterInterface(QObject *object)
-: QObject(object)
+NetWorkReceiver::SendType MediaUpdaterInterface::processMessage(NetworkMessageReader* msg)
 {
-    // Make compiler generate proper template
-    sendOffChanges<double>(nullptr, QString());
-    sendOffChanges<int>(nullptr, QString());
-    sendOffChanges<QColor>(nullptr, QString());
-    sendOffChanges<Core::ScaleUnit>(nullptr, QString());
-    sendOffChanges<QString>(nullptr, QString());
-    sendOffChanges<Core::ScaleUnit>(nullptr, QString());
-    sendOffChanges<Core::Layer>(nullptr, QString());
-    sendOffChanges<Core::GridPattern>(nullptr, QString());
-    sendOffChanges<Core::PermissionMode>(nullptr, QString());
-    sendOffChanges<Core::VisibilityMode>(nullptr, QString());
-    sendOffChanges<bool>(nullptr, QString());
+    return NetWorkReceiver::NONE;
 }
-
-template<typename T>
-void MediaUpdaterInterface::sendOffChanges(MediaControllerBase *ctrl, const QString &property)
-{
-    if(nullptr == ctrl || m_updatingFromNetwork)
-        return;
-
-    NetworkMessageWriter msg(NetMsg::MediaCategory, NetMsg::UpdateMediaProperty);
-    msg.uint8(static_cast<int>(ctrl->contentType()));
-    msg.string8(ctrl->uuid());
-    msg.string16(property);
-    auto val= ctrl->property(property.toLocal8Bit().data());
-    Helper::variantToType<T>(val.value<T>(), msg);
-    msg.sendToServer();
-}
-
-
--- core/src/updater/media/mediaupdaterinterface.h
+++ core/src/updater/media/mediaupdaterinterface.h
@@ -20,22 +20,42 @@
 #ifndef MEDIAUPDATERINTERFACE_H
 #define MEDIAUPDATERINTERFACE_H
 
+#include "network/receiveevent.h"
 #include <QObject>
 
+#include "network/networkmessagereader.h"
+#include "network/networkmessagewriter.h"
+#include "worker/convertionhelper.h"
+
 class MediaControllerBase;
-class MediaUpdaterInterface : public QObject
+class MediaUpdaterInterface : public QObject, public NetWorkReceiver
 {
     Q_OBJECT
 public:
-    MediaUpdaterInterface(QObject* object = nullptr);
-    virtual void addMediaController(MediaControllerBase* ctrl) = 0;
-
+    MediaUpdaterInterface(QObject* object= nullptr);
+    virtual void addMediaController(MediaControllerBase* ctrl)= 0;
 
     template <typename T>
     void sendOffChanges(MediaControllerBase* ctrl, const QString& property);
+
+    NetWorkReceiver::SendType processMessage(NetworkMessageReader* msg) override;
 
 protected:
     bool m_updatingFromNetwork= false;
 };
 
+template <typename T>
+void MediaUpdaterInterface::sendOffChanges(MediaControllerBase* ctrl, const QString& property)
+{
+    if(nullptr == ctrl || m_updatingFromNetwork)
+        return;
+
+    NetworkMessageWriter msg(NetMsg::MediaCategory, NetMsg::UpdateMediaProperty);
+    msg.uint8(static_cast<int>(ctrl->contentType()));
+    msg.string8(ctrl->uuid());
+    msg.string16(property);
+    auto val= ctrl->property(property.toLocal8Bit().data());
+    Helper::variantToType<T>(val.value<T>(), msg);
+    msg.sendToServer();
+}
 #endif // MEDIAUPDATERINTERFACE_H
