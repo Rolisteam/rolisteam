e2f0603e01cb9e0432365006d53b19dc884eb8a2
Renaud G <renaud@rolisteam.org>
2023-11-06 23:22:11 +0100

    Update message.sh to get texts from ui files.

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-#Make sure UI Files are translated as well
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp
-
 #Extract strings
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.qml | grep -v "libraries/diceparser"` -o $podir/rolisteam_qt.pot
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.qml  -o -name \*.ui | grep -v "libraries/diceparser"` -o $podir/rolisteam_qt.pot
